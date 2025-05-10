21b40cd8a49e8eb9354786e4976ad82a6a71ce22
Timur Kerimov <tim-online@mail.ru>
2025-05-10 08:45:36 +0300

    Revert "Adds search history feature with navigation and management controls"
    This reverts commit 053affbff530a57f25dbf24a70fed88368ef17e5.

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,4 +2,3 @@ export {useUser} from './useUser';
 export {useProviders} from './useProviders';
 export {useTariffs} from './useTariffs';
 export {useForm} from './useForm';
-export {useSearchHistory} from './useSearchHistory';
